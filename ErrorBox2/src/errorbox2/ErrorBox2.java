4c46cf657eb912f6cd219586b17d15d835894787
Brian Whitney Fristensky <frist@jupiter.cc.umanitoba.ca>
2022-07-23 15:51:24 -0500

    Added a comment to ErrorBox2.java as a test

--- ErrorBox2/src/errorbox2/ErrorBox2.java
+++ ErrorBox2/src/errorbox2/ErrorBox2.java
@@ -1,8 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
+/* * To change this license header, choose License Headers in Project Properties.  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/* THIS COMMENT ADDED BY BF  TO TEST GIT */
 package errorbox2;
 
 import javax.swing.JOptionPane;
